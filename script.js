73e64c474368ff293c44859f6f0ca765bc589bf4
NitinJuyal1610 <nitinjuyal1610@gmail.com>
2021-11-17 16:04:28 +0530

    project bloodDr

---
+++ script.js
@@ -0,0 +1,31 @@
+function Slider(n) {
+  for (let i = 1; i <= 3; i++) {
+    if (i === n) {
+      document.querySelector(`.myslider${n}`).style.display = "block";
+    } else {
+      document.querySelector(`.myslider${i}`).style.display = "none";
+    }
+  }
+}
+
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("slide");
+  var dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+  setTimeout(showSlides, 9000); // Change image every 2 seconds
+}
